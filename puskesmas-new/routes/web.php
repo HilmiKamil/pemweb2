4b7c6de2396235cc11c083c7170b0c6135d512af
HilmiKamil <hilmihelios@gmail.com>
2024-05-17 10:49:32 +0700

    Praktikum Laravel 2

--- puskesmas-new/routes/web.php
+++ puskesmas-new/routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AboutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,3 +21,11 @@ Route::get('/salam', function () {
 Route::get('/profile', function () {
     return view('Halaman Profile');
 });
+
+
+
+//Praktikum Laravel 2
+
+Route::get('/dashboard', [AdminController::class, 'index']);
+Route::get('/contact', [ContactController::class, 'index']);
+Route::get('/about', [AboutController::class, 'index']);
